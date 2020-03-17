543913adb3247ee3241d78a3fde5b69cd25573f9
Brett Sevy <justbrettt@gmail.com>
2020-03-17 08:25:36 -0500

    fixed some heroku problems

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,36 +1,35 @@
 var CACHE_NAME = 'cache-v1';
 
 var urlsToCache = [
-  "/",
-  "/styles.css",
-  "/index.js",
-  "/index.html",
-  "/db.js",
-  "/assets/images/icons/favicon-192x192.png",
-  "/assets/images/icons/favicon-512x512.png",
+    "/db.js",
+    "/index.html",
+    "/index.js",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
 ];
 
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get("accept").includes("text/html")) {
-          return caches.match("/index.html");
-        }
-      });
-    })
-  );
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else if (event.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/index.html");
+                }
+            });
+        })
+    );
 });
